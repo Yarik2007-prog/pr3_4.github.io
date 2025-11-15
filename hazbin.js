88f24372983b37ceecc6df2dae4c40420d1ba719
Yarik2007-prog <150480330+Yarik2007-prog@users.noreply.github.com>
2025-11-15 13:54:41 +0200

    Add files via upload

---
+++ hazbin.js
@@ -0,0 +1,190 @@
+export const pokemons = [
+    {
+        name: 'Alastor',
+        hp: 274,
+        img: 'assets/3F3F3F_.png',
+        attacks: [
+            {
+                name: "static jolt",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "spark orb",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "volt tackle",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "lightning surge",
+                maxDamage: 160,
+                minDamage: 130,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'assets/3F3F3F3F3F3F3F3F3F.webp',
+        name: 'Lucifer',
+        hp: 282,
+        attacks: [
+            {
+                name: "ember",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "flamethrower",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "inferno tail",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "blaze vortex",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'assets/val.webp',
+        name: 'Valentino',
+        hp: 294,
+        attacks: [
+            {
+                name: "tackle",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "vine lash",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "razor leaf",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "solar beam",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'assets/vel.webp',
+        name: 'Velvete',
+        type: 'water',
+        hp: 292,
+        attacks: [
+            {
+                name: "aqua bubble",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "torrent shot",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "shell bash",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "tsunami pump",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'assets/adam.png',
+        name: 'Adam',
+        hp: 284,
+        attacks: [
+            {
+                name: "gale slash",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "razor wind",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "searing gust",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "aerial strike",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+
+    {
+        img: 'assets/Vox.webp',
+        name: 'Vox',
+        hp: 284,
+        attacks: [
+            {
+                name: "gale slash",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "razor wind",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "searing gust",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "aerial strike",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    }
+];
